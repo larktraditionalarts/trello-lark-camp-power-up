62efe69f040345828c829daefd82aad572f984e5
Will Wheeler <willfulbard@gmail.com>
2022-12-01 12:58:53 -0800

    Updates

---
+++ change-date-dialog.5f6762a2.js
@@ -0,0 +1,18 @@
+const $934e8ad3e8eb8cf3$var$t = TrelloPowerUp.iframe();
+const $934e8ad3e8eb8cf3$var$finish = ()=>{
+    $934e8ad3e8eb8cf3$var$t.closePopup();
+};
+window.addYear = ()=>{
+    alert("add year");
+    $934e8ad3e8eb8cf3$var$finish();
+};
+window.removeYear = ()=>{
+    alert("remove year");
+    $934e8ad3e8eb8cf3$var$finish();
+};
+$934e8ad3e8eb8cf3$var$t.render(()=>{
+    $934e8ad3e8eb8cf3$var$t.sizeTo("#edit-date").done();
+});
+
+
+//# sourceMappingURL=change-date-dialog.5f6762a2.js.map

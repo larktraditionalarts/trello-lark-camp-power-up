a08ff2273204eee1d178dea79dd472ab160735ea
Will Wheeler <willfulbard@gmail.com>
2022-12-01 18:40:49 -0800

    Updates

--- index.c1b74552.js
+++ index.c1b74552.js
@@ -63,7 +63,7 @@ $7d39d93f9098a310$export$f7ad0328861e2f03 = $7d39d93f9098a310$var$resolve;
 
 var $348644c521010b85$exports = {};
 
-(parcelRequire("aKzDW")).register(JSON.parse('{"aykEV":"index.00e0e53f.js","7YnGh":"calendar-week-black.fe141c96.svg","lYzdV":"calendar-week-white.7a92156f.svg"}'));
+(parcelRequire("aKzDW")).register(JSON.parse('{"aykEV":"index.c1b74552.js","7YnGh":"calendar-week-black.fe141c96.svg","lYzdV":"calendar-week-white.7a92156f.svg"}'));
 
 var $4fec959f05098ead$exports = {};
 
@@ -81,7 +81,7 @@ const $1e64527cd714425c$var$icons = {
         dark: new URL($4671090a148729ab$exports).href
     }
 };
-console.log("with edit version 7");
+console.log("Lark Camp PowerUp v7");
 TrelloPowerUp.initialize({
     "card-buttons": (t)=>[],
     "board-buttons": ()=>{
@@ -102,4 +102,4 @@ TrelloPowerUp.initialize({
 });
 
 
-//# sourceMappingURL=index.00e0e53f.js.map
+//# sourceMappingURL=index.c1b74552.js.map

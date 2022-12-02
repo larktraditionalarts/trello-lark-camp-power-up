4a5018d41c06e20540b1e9a91694ca5cc5cd06b1
Will Wheeler <willfulbard@gmail.com>
2022-12-01 17:16:57 -0800

    Updates

--- change-date-dialog.172f2bb8.js
+++ change-date-dialog.172f2bb8.js
@@ -2,15 +2,25 @@ const $934e8ad3e8eb8cf3$var$t = TrelloPowerUp.iframe();
 const $934e8ad3e8eb8cf3$var$finish = ()=>{
     $934e8ad3e8eb8cf3$var$t.closePopup();
 };
+const $934e8ad3e8eb8cf3$var$toggleLoad = ()=>{
+    [
+        "edit-date",
+        "loading"
+    ].forEach((id)=>{
+        const el = document.getElementById(id);
+        el.className = el.className === "hidden" ? "" : "hidden";
+    });
+};
 window.addYear = ()=>{
+    $934e8ad3e8eb8cf3$var$toggleLoad();
     $934e8ad3e8eb8cf3$var$t.cards("all").then((cards)=>{
         console.log(JSON.stringify(cards, null, 2));
+        // t.set(card.id, 'shared', '', value)
+        $934e8ad3e8eb8cf3$var$toggleLoad();
     });
-    $934e8ad3e8eb8cf3$var$finish();
 };
 window.removeYear = ()=>{
     alert("remove year");
-    $934e8ad3e8eb8cf3$var$finish();
 };
 window.unsetCompleted = ()=>{
     alert("unset completed");
@@ -24,4 +34,4 @@ $934e8ad3e8eb8cf3$var$t.render(()=>{
 });
 
 
-//# sourceMappingURL=change-date-dialog.1b29d6f4.js.map
+//# sourceMappingURL=change-date-dialog.172f2bb8.js.map
